ae138b32c2672a927457cc0fb5f2738e826872d8
lcj1992 <1227627228@qq.com>
2016-01-24 11:00:32 +0800

    git

--- java/src/main/java/annotation/fruit/FruitInfoUtil.java
+++ java/src/main/java/annotation/fruit/FruitInfoUtil.java
@@ -16,15 +16,15 @@ public class FruitInfoUtil {
 
         for (Field field : fields) {
             if (field.isAnnotationPresent(FruitName.class)) {
-                FruitName fruitName = (FruitName) field.getAnnotation(FruitName.class);
+                FruitName fruitName = field.getAnnotation(FruitName.class);
                 strFruitName = strFruitName + fruitName.value();
                 System.out.println(strFruitName);
             } else if (field.isAnnotationPresent(FruitColor.class)) {
-                FruitColor fruitColor = (FruitColor) field.getAnnotation(FruitColor.class);
+                FruitColor fruitColor = field.getAnnotation(FruitColor.class);
                 strFruitColor = strFruitColor + fruitColor.fruitColor().toString();
                 System.out.println(strFruitColor);
             } else if (field.isAnnotationPresent(FruitProvider.class)) {
-                FruitProvider fruitProvider = (FruitProvider) field.getAnnotation(FruitProvider.class);
+                FruitProvider fruitProvider = field.getAnnotation(FruitProvider.class);
                 strFruitProvicer = strFruitProvicer + " 供应商编号：" + fruitProvider.id() + " 供应商名称：" + fruitProvider.name()
                         + " 供应商地址：" + fruitProvider.address();
                 System.out.println(strFruitProvicer);

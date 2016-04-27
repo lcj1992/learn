06afc43b744e8f3cceb563d530af14c1c002e512
lcj1992 <1227627228@qq.com>
2016-04-27 18:46:10 +0800

    protobuf helloworld

--- java/src/main/java/jdk/classLoader/Test.java
+++ java/src/main/java/jdk/classLoader/Test.java
@@ -1,14 +1,18 @@
 package jdk.classLoader;
 
+import java.util.Date;
+
 /**
  * Created by lcj on 15-5-16.
  */
 public class Test {
 
     public static void main(String[] args) throws ClassNotFoundException {
-        FileClassLoader fileClassLoader = new FileClassLoader("/home/lcj/work/simple/lcj-java/target/classes");
-        String className = "Apple";
-        Class clazz = fileClassLoader.loadClass(className);
-        System.out.println(clazz.getClassLoader());
+//        FileClassLoader fileClassLoader = new FileClassLoader("/home/lcj/work/simple/lcj-java/target/classes");
+//        String className = "Apple";
+//        Class clazz = fileClassLoader.loadClass(className);
+//        System.out.println(clazz.getClassLoader());
+
+        System.out.println(new Date(1252639886000L));
     }
 }
--- java/src/main/java/test/JustTest.java
+++ java/src/main/java/test/JustTest.java
@@ -12,39 +12,39 @@ import java.util.Map;
  */
 public class JustTest {
     public static void main(String[] args) {
-//        HashMap<String, Integer> map = new HashMap<String, Integer>();
-//        map.put("语文", 1);
-//        map.put("数学", 2);
-//        map.put("英语", 3);
-//        map.put("历史", 4);
-//        map.put("政治", 5);
-//        map.put("地理", 6);
-//        map.put("生物", 7);
-//        map.put("aa", 8);
-//        map.put("bb", 8);
-//        map.put("cc", 8);
-//        map.put("dd", 8);
-//        map.put("ee", 8);
-//        map.put("ff", 8);
-//        map.put("gg", 8);
-//        map.put("hh", 8);
-//        map.put("ii", 8);
-//        map.put("jj", 8);
-//        map.put("oo", 8);
-//        map.put("zz", 8);
-//        map.put("kk", 8);
-//        System.out.println(map.size());
-//        System.out.println(map.entrySet().size());
-//        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-//            System.out.println(entry.getKey() + ": " + entry.getValue());
-//        }
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("语文", 1);
+        map.put("数学", 2);
+        map.put("英语", 3);
+        map.put("历史", 4);
+        map.put("政治", 5);
+        map.put("地理", 6);
+        map.put("生物", 7);
+        map.put("aa", 8);
+        map.put("bb", 8);
+        map.put("cc", 8);
+        map.put("dd", 8);
+        map.put("ee", 8);
+        map.put("ff", 8);
+        map.put("gg", 8);
+        map.put("hh", 8);
+        map.put("ii", 8);
+        map.put("jj", 8);
+        map.put("oo", 8);
+        map.put("zz", 8);
+        map.put("kk", 8);
+        System.out.println(map.size());
+        System.out.println(map.entrySet().size());
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
-        List<Integer> list = Lists.newArrayList();
-        testUnBounded(list);
-        testBounded(list);
-        add1(list,1);
-        add2(list,1);
-        add3(list,1);
+//        List<Integer> list = Lists.newArrayList();
+//        testUnBounded(list);
+//        testBounded(list);
+//        add1(list,1);
+//        add2(list,1);
+//        add3(list,1);
     }
 
 

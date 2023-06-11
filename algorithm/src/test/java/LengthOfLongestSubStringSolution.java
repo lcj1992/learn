444ede3cd80dc3a2c1a88045f8a2a3f074170713
foolchild <1227627228@qq.com>
2023-06-11 23:49:07 +0800

    rename

---
+++ algorithm/src/test/java/LengthOfLongestSubStringSolution.java
@@ -0,0 +1,42 @@
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author lichuangjian
+ * @date 2023/6/11
+ */
+public class LengthOfLongestSubStringSolution {
+
+    public static void main(String[] args) {
+        LengthOfLongestSubStringSolution solution = new LengthOfLongestSubStringSolution();
+        String s = "abcabcbb";
+        int i = solution.lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+        if (length <= 0) {
+            return 0;
+        }
+        int maxLength = 0;
+        char[] chars = s.toCharArray();
+        List<Character> results = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            char aChar = chars[i];
+            if (results.contains(aChar)) {
+                maxLength = Math.max(maxLength, results.size());
+                // 找到该字符坐标
+                int index = results.indexOf(aChar);
+                results = results.subList(index + 1, results.size());
+            }
+            results.add(aChar);
+        }
+        maxLength = Math.max(maxLength, results.size());
+        return maxLength;
+    }
+}
---
+++ algorithm/src/test/java/ReverseSolution.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lichuangjian
+ * @date 2023/6/11
+ */
+public class ReverseSolution {
+
+    public static void main(String[] args) {
+        ReverseSolution solution = new ReverseSolution();
+        int reverse = solution.reverse(-1);
+        System.out.println(reverse);
+    }
+
+    public int reverse(int x) {
+        int flag = 1;
+        long lo = x;
+        if (lo < 0) {
+            lo = lo * -1;
+            flag = -1;
+        }
+        List<Long> nums = new ArrayList<>();
+        if (lo < 10) {
+            return (int) lo * flag;
+        }
+        if (lo == 10) {
+            return flag;
+        }
+        long temp = lo;
+        while (temp >= 10) {
+            long num = temp % 10;
+            nums.add(num);
+            temp = temp / 10;
+        }
+        nums.add(temp);
+        long result = 0;
+        int i = nums.size();
+        for (Long num : nums) {
+            if (num == 0) {
+                i--;
+                continue;
+            }
+            result += num * Math.pow(10, i - 1);
+            i--;
+        }
+        result = result * flag;
+        if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE - 1) {
+            return 0;
+        }
+        return (int) result;
+    }
+}

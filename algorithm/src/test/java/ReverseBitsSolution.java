fbd270c1a34d8a7aa1de0ebe190c00c3553ae193
foolchild <1227627228@qq.com>
2023-08-28 08:09:47 +0800

    rename

---
+++ algorithm/src/test/java/ReverseBitsSolution.java
@@ -0,0 +1,15 @@
+/**
+ * @author lichuangjian
+ * @date 2023/8/24
+ */
+public class ReverseBitsSolution {
+
+    public static void main(String[] args) {
+        ReverseBitsSolution solution = new ReverseBitsSolution();
+        solution.reverseBits(1111101);
+    }
+
+    public int reverseBits(int n) {
+        return 0;
+    }
+}
--- algorithm/src/test/java/common/ListNode.java
+++ algorithm/src/test/java/common/ListNode.java
@@ -1,5 +1,7 @@
 package common;
 
+import java.util.Objects;
+
 public class ListNode {
     public int val;
     public ListNode next;
@@ -14,5 +16,26 @@ public class ListNode {
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+
+    public static void print(ListNode node) {
+        if (Objects.isNull(node)) {
+            return;
+        }
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    public static ListNode createFromArray(int[] nums) {
+        int first = nums[0];
+        ListNode head = new ListNode(first);
+        ListNode temp = head;
+        for (int i = 1; i < nums.length; i++) {
+            temp.next = new ListNode(nums[i]);
+            temp = temp.next;
+        }
+        return head;
     }
 }

fa887c09f2730b8bed3a258a041903fb2b4b00fa
lcj1992 <1227627228@qq.com>
2024-05-20 09:04:33 +0800

    rename

--- algorithm/src/test/java/Interview.java
+++ algorithm/src/test/java/Interview.java
@@ -6,20 +6,27 @@ public class Interview {
     // LargestNumberSolutionTest，最大数，本质上还是排序，自定义排序逻辑而已
     // testGenerateMatrix，给定数组打印矩阵
     // BinaryTreeTest，二叉树的前中后序遍历、层序遍历、是否子树、是否完全相同、共同子结构
+    // TODO 镜像二叉树 Java 反射
+    // TODO 从右边看二叉树输出；平衡二叉树，优化
+    // TODO 找出二叉树中所有路径节点值相加为指定值的路径
+    // TODO 二叉树中，查找所有根到叶子节点的和为 target 的路径
     // TODO 两棵二叉树，寻找他们共同子结构，给出相同子结构的节点数量。
     // tibcacbdata，问能否在这个字符串中找到一个长度为m的连续子串，使得这个子串刚好由上面m个字符组成，顺序无所谓，返回任意满足条件的一个子串的起始位置，未找到返回-1，时间复杂度On，比如上面这个例子，acbd，3。
-    // kthToLastSolution，链表中倒数第k个元素
-    // 奇数位升序偶数位降序链表排序
+    // kthToLastSolution，链表中倒数第k个元素，双指针
+    // TODO 奇数位升序偶数位降序链表排序
     // RemoveZeroSumSublistsSolution，移除链表中连续和为0的元素，前缀和
     // ReverseListSolution，反转链表
     // ReverseKGroupSolutionTest，间隔k位反转链表，间隔2位反转链表
     // TODO 反转链表，left到right之间的
-    // TODO 两个链表相乘
+    // TODO 两个链表相乘，1->9->null,3->2->null 返回 6->0->8 不能遍历后变成整数直接计算
     // TODO 约瑟夫问题
     // SearchSolution，搜索旋转排序数组
     // SearchRangeSolution，二分查找变种，两种思路，一种是找到后暴力探索
     // LengthOfLongestSubstringKDistinctSolution，和最长无重复子串类似
     // LengthOfLongestSubStringSolution，map记录出现过的字符，调整left指针，最大长度即为当前值和（当前指针-left指针+1）两者的最大值
     // ContainCharArrayTest，问能否在这个字符串中找到一个长度为m的连续子串
-    // 01背包
+    // TODO 01背包
+    // TODO 回溯
+    // TODO 最长上升子序列
+    // TODO 最长公共子序列
 }

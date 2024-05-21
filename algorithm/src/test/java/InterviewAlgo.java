c8f0ee9d30cabc84aa1cc94367179013ffd28296
lcj1992 <1227627228@qq.com>
2024-05-21 20:00:22 +0800

    反转链表

--- algorithm/src/test/java/InterviewAlgo.java
+++ algorithm/src/test/java/InterviewAlgo.java
@@ -9,6 +9,7 @@ public class InterviewAlgo {
     // 10 11 15 16
     // TODO 数组中元素与下一个比它大的元素之间的距离
 
+
     // 线性表
     // RemoveZeroSumSublistsSolution，移除链表中连续和为0的元素，前缀和，https://leetcode.cn/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
     // ReverseListSolution，反转链表，https://leetcode.cn/problems/UHnkqh/description/
@@ -21,6 +22,7 @@ public class InterviewAlgo {
     // TODO LRU缓存机制
     // TODO 找两个队列里的相同元素
     // MergeKListsTest，合并多个有序链表，merge k sorted list，https://leetcode.cn/problems/merge-k-sorted-lists/description/
+
 
     // 图
     // GraphTest，给定微服务集群中的调用关系，例如：[('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'A')]，看是否存在循环调用
@@ -39,20 +41,24 @@ public class InterviewAlgo {
     // TODO 两棵二叉树，寻找他们共同子结构，给出相同子结构的节点数量
     // TODO 二叉树其他题目，https://blog.csdn.net/LaoJiu_/article/details/66974736
 
+
     // 排序
     // TODO 12. 在有序数组里查询一个数，数组里数相等的情况取最小 idx
     // LargestNumberSolutionTest，最大数，本质上还是排序，自定义排序逻辑而已。给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数，https://leetcode.cn/problems/largest-number/solutions/27403/179-by-ikaruga/
+
 
     // 查找
     // SearchSolution，搜索旋转排序数组，https://leetcode.cn/problems/search-in-rotated-sorted-array
     // SearchMatrixSolution，搜索二维矩阵，https://leetcode.cn/problems/search-a-2d-matrix/
     // SearchRangeSolution，给定一个升序整型数组，如[1,2,2,3,4,5,5,6]，给定一个target，找到开始index和结束index，如target=2返回[1,2]，target=3返回[3,3]，如果target不存在，则返回[-1,-1]，如target=7返回[-1,-1]。二分查找变种，两种思路，一种是找到后暴力探索，https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
 
+
     // 双指针，滑动窗口
     // LengthOfLongestSubstringKDistinctSolution，至多包含 K 个不同字符的最长子串，和最长无重复子串类似，https://leetcode.cn/problems/longest-substring-with-at-most-k-distinct-characters/description/
     // LengthOfLongestSubStringSolution，map记录出现过的字符，调整left指针，最大长度即为当前值和（当前指针-left指针+1）两者的最大值；要求输出重复最长子串 https://leetcode.cn/problems/longest-substring-without-repeating-characters/solutions/3982/hua-dong-chuang-kou-by-powcai/
     // ContainCharArrayTest，tibcacbdata，问能否在这个字符串中找到一个长度为m的连续子串，使得这个子串刚好由上面m个字符组成，顺序无所谓，返回任意满足条件的一个子串的起始位置，未找到返回-1，时间复杂度On，比如上面这个例子，acbd，3。https://cloud.tencent.com/developer/article/2285729
     // kthToLastSolution，链表中倒数第k个元素，双指针
+
 
     // 动态规划
     // Knapsack01Test，01背包问题，状态转移方程：dp[i][j] = Math.max(dp[i - 1][j], (v[i - 1] + dp[i - 1][j - w[i - 1]])); dp[i][j] = dp[i-1][j]
@@ -61,6 +67,7 @@ public class InterviewAlgo {
     // LongestPalindromeTest，最长回文子串
     // FindNumberOfLISTest
 
+
     // TODO 回溯
     // TODO 全排列
     // TODO N皇后
@@ -68,6 +75,7 @@ public class InterviewAlgo {
 
     // 设计模式
     // DoubleCheckVersion，单例模式
+
 
     // 其他
     // Three6AddSolution，36进制加法

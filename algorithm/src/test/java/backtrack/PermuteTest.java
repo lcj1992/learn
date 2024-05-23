5099360566eb2ea84063cc7232db101a20d93f70
lcj1992 <1227627228@qq.com>
2024-05-23 08:14:44 +0800

    全排列，n皇后

---
+++ algorithm/src/test/java/backtrack/PermuteTest.java
@@ -0,0 +1,46 @@
+package backtrack;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * <a href="https://leetcode.cn/problems/permutations/description/">...</a>
+ * 全排列
+ * 思路：回溯
+ * <p>
+ * author: foolchild
+ * date: 2019/5/18
+ */
+public class PermuteTest {
+
+    @Test
+    public void test() {
+        List<List<Integer>> permute = permute(new int[]{1, 2, 3});
+        permute.forEach(System.out::println);
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> list = new ArrayDeque<>();
+        dfs(res, nums, list);
+        return res;
+    }
+
+    public void dfs(List<List<Integer>> res, int[] nums, Deque<Integer> list) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int num : nums) {
+            if (list.contains(num)) {
+                continue;
+            }
+            list.addLast(num);
+            dfs(res, nums, list);
+            list.removeLast();
+        }
+    }
+
+}

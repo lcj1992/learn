adc19609e8f1fa2058f57638f5115fdfc4dae65c
lcj1992 <1227627228@qq.com>
2024-05-28 09:28:39 +0800

    几道题

--- algorithm/src/test/java/InterviewAlgo.java
+++ algorithm/src/test/java/InterviewAlgo.java
@@ -297,4 +297,34 @@ public class InterviewAlgo {
     链接：https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/solutions/255811/cong-qian-xu-yu-zhong-xu-bian-li-xu-lie-gou-zao-9/
     来源：力扣（LeetCode）
     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+    给你两个整数 a 和 b ，不使用 运算符 + 和 - ​​​​​​​，计算并返回两整数之和。
+
+
+
+    示例 1：
+
+    输入：a = 1, b = 2
+    输出：3
+    示例 2：
+
+    输入：a = 2, b = 3
+    输出：5
+
+    class Solution {
+        public int getSum(int a, int b) {
+            while (b != 0) {
+                int carry = (a & b) << 1;
+                a = a ^ b;
+                b = carry;
+            }
+            return a;
+        }
+    }
+
+    作者：力扣官方题解
+    链接：https://leetcode.cn/problems/sum-of-two-integers/solutions/1017617/liang-zheng-shu-zhi-he-by-leetcode-solut-c1s3/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 }

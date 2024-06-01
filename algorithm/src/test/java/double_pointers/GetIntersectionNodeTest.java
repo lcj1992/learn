d875380adab9822fb181982575642877c390b86d
lcj1992 <1227627228@qq.com>
2024-06-01 16:58:38 +0800

    Krahets 笔面试精选 88 题

--- algorithm/src/test/java/double_pointers/GetIntersectionNodeTest.java
+++ algorithm/src/test/java/double_pointers/GetIntersectionNodeTest.java
@@ -17,9 +17,23 @@ public class GetIntersectionNodeTest {
 
     @Test
     public void test() {
-        ListNode oneNode = ListNode.createFromArray(4, 1, 8, 4, 5);
-        ListNode one2Node = ListNode.createFromArray(5, 6, 1);
-        ListNode intersectionNode = getIntersectionNode(oneNode, one2Node);
+        ListNode node1 = new ListNode(4);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(8);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(5);
+        ListNode node7 = new ListNode(6);
+        ListNode node8 = new ListNode(1);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node6.next = node7;
+        node7.next = node8;
+        node8.next = node3;
+
+        ListNode intersectionNode = getIntersectionNode2(node1, node6);
         System.out.println(intersectionNode.val);
     }
 
@@ -42,11 +56,15 @@ public class GetIntersectionNodeTest {
     }
 
     public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
-        ListNode A = headA, B = headB;
-        while (A != B) {
-            A = A != null ? A.next : headB;
-            B = B != null ? B.next : headA;
+        ListNode nodeA = headA;
+        ListNode nodeB = headB;
+        // 两次遍历最终会在交汇点重合
+        while (nodeA != nodeB) {
+            // 先遍历A链表，后遍历B链表
+            nodeA = nodeA != null ? nodeA.next : headB;
+            // 先遍历B链表，后遍历A链表
+            nodeB = nodeB != null ? nodeB.next : headA;
         }
-        return A;
+        return nodeA;
     }
 }

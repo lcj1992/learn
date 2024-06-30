6f6124353638e8bddcdb28a6054a31728ce66be6
lcj1992 <1227627228@qq.com>
2024-06-30 14:50:47 +0800

    死锁测试，lock和synchronized的区别

--- design_pattern/src/test/java/creational/abstractFactory/AbstractFactoryTest.java
+++ design_pattern/src/test/java/creational/abstractFactory/AbstractFactoryTest.java
@@ -1,84 +1,14 @@
 package creational.abstractFactory;
 
+import org.junit.Test;
+
 /**
  * Created by lcj on 15-10-31.
  */
-//GuiFactory example
-
-//Abstract Product
-interface Product1 {
-    void paint();
-}
-
-//Abstract product
-interface Product2 {
-    void paint();
-}
-
-//Abstract Factory
-interface Factory {
-    Product1 createProduct1();
-
-    Product2 createProduct2();
-}
-
-//Concrete Factory
-class Factory1 implements Factory {
-    public Product1 createProduct1() {
-        return new ConcreteProduct1_2();
-    }
-
-    public Product2 createProduct2() {
-        return new ConcreteProduct2_2();
-    }
-}
-
-//Concrete Factory
-class Factory2 implements Factory {
-    public Product1 createProduct1() {
-        return new ConcreteProduct1_1();
-    }
-
-    public Product2 createProduct2() {
-        return new ConcreteProduct2_1();
-    }
-
-}
-
-//Concrete Product
-class ConcreteProduct1_1 implements Product1 {
-    public void paint() {
-        System.out.println("I'm an ConcreteProduct1_1");
-    }
-}
-
-//Concrete Product
-class ConcreteProduct1_2 implements Product1 {
-    public void paint() {
-        System.out.println("I'm a ConcreteProduct1_2");
-    }
-}
-
-//Concrete Product
-class ConcreteProduct2_1 implements Product2 {
-    public void paint() {
-        System.out.println("I'm an ConcreteProduct2_1");
-    }
-}
-
-//Concrete Product
-class ConcreteProduct2_2 implements Product2 {
-    public void paint() {
-        System.out.println("I'm a ConcreteProduct2_2");
-    }
-}
-
-//Client application is not aware about the how the product is created. Its only responsible to give a name of
-//concrete staticfactory
 public class AbstractFactoryTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void test() {
         Factory factory = new Factory1();
 
         // 产品族  button和label
@@ -87,7 +17,69 @@ public class AbstractFactoryTest {
 
         product1.paint();
         product2.paint();
+    }
 
+
+    //Abstract Product
+    interface Product1 {
+        void paint();
+    }
+
+    //Abstract product
+    interface Product2 {
+        void paint();
+    }
+
+    //Abstract Factory
+    interface Factory {
+        Product1 createProduct1();
+
+        Product2 createProduct2();
+    }
+
+    static class Factory1 implements Factory {
+        public Product1 createProduct1() {
+            return new ConcreteProduct1_2();
+        }
+
+        public Product2 createProduct2() {
+            return new ConcreteProduct2_2();
+        }
+    }
+
+    static class Factory2 implements Factory {
+        public Product1 createProduct1() {
+            return new ConcreteProduct1_1();
+        }
+
+        public Product2 createProduct2() {
+            return new ConcreteProduct2_1();
+        }
+
+    }
+
+    static class ConcreteProduct1_1 implements Product1 {
+        public void paint() {
+            System.out.println("I'm an ConcreteProduct1_1");
+        }
+    }
+
+    static class ConcreteProduct1_2 implements Product1 {
+        public void paint() {
+            System.out.println("I'm a ConcreteProduct1_2");
+        }
+    }
+
+    static class ConcreteProduct2_1 implements Product2 {
+        public void paint() {
+            System.out.println("I'm an ConcreteProduct2_1");
+        }
+    }
+
+    static class ConcreteProduct2_2 implements Product2 {
+        public void paint() {
+            System.out.println("I'm a ConcreteProduct2_2");
+        }
     }
 }
 
--- design_pattern/src/test/java/creational/facotry/FactoryTest.java
+++ design_pattern/src/test/java/creational/facotry/FactoryTest.java
@@ -1,33 +1,38 @@
 package creational.facotry;
 
+import org.junit.Test;
+
 /**
  * Created by lcj on 15-11-8.
  */
 
-interface IProduct {
-    void productMethod();
-}
-
-class ConcreteProduct implements IProduct {
-    public void productMethod() {
-        System.out.println("产品1");
-    }
-}
-
-interface IFactory {
-    IProduct createProduct();
-}
-
-class ConcreteFactory implements IFactory {
-    public IProduct createProduct() {
-        return new ConcreteProduct();
-    }
-}
 
 public class FactoryTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void test() {
         IFactory factory = new ConcreteFactory();
         IProduct product = factory.createProduct();
         product.productMethod();
+    }
+
+    interface IProduct {
+        void productMethod();
+    }
+
+    static class ConcreteProduct implements IProduct {
+        public void productMethod() {
+            System.out.println("产品1");
+        }
+    }
+
+    interface IFactory {
+        IProduct createProduct();
+    }
+
+    static class ConcreteFactory implements IFactory {
+        public IProduct createProduct() {
+            return new ConcreteProduct();
+        }
     }
 }

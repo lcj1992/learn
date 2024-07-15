66ee3c05163d59270954d4f29dff90f7e2fdc928
lcj1992 <1227627228@qq.com>
2024-07-15 20:08:24 +0800

    leetcode

--- algorithm/src/test/java/algo/greedy/MaxChunksToSortedTest.java
+++ algorithm/src/test/java/algo/greedy/MaxChunksToSortedTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 
 /**
  * <a href="https://leetcode.cn/problems/max-chunks-to-make-sorted-ii/">...</a>
@@ -16,11 +15,11 @@ public class MaxChunksToSortedTest {
 
     @Test
     public void test() {
-        int res = maxChunksToSorted2(new int[]{1, 1, 2, 1, 1, 3, 4, 5, 1, 6});
+        int res = maxChunksToSorted(new int[]{1, 1, 2, 1, 1, 3, 4, 5, 1, 6});
         System.out.println(res);
     }
 
-    public int maxChunksToSorted2(int[] arr) {
+    public int maxChunksToSorted(int[] arr) {
         Deque<Integer> deque = new ArrayDeque<>();
         for (Integer num : arr) {
             // 栈不为空，且栈顶元素大于num
@@ -37,19 +36,4 @@ public class MaxChunksToSortedTest {
         return deque.size();
     }
 
-    public int maxChunksToSorted(int[] arr) {
-        LinkedList<Integer> stack = new LinkedList<>();
-        for (int num : arr) {
-            if (!stack.isEmpty() && num < stack.getFirst()) {
-                int head = stack.removeFirst();
-                while (!stack.isEmpty() && num < stack.getFirst()) {
-                    stack.removeFirst();
-                }
-                stack.addFirst(head);
-                continue;
-            }
-            stack.addFirst(num);
-        }
-        return stack.size();
-    }
 }
